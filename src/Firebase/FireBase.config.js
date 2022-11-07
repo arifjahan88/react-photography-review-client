afd11d63a4b806b4ad633faf00cf1f738852b28f
Arif Jahan <arifjahan88@gmail.com>
2022-11-08 02:54:06 +0600

    after add environment key

--- src/Firebase/FireBase.config.js
+++ src/Firebase/FireBase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBDYHm7uNCGZtTdWyJSWnD55L-IQ4r4Q1g",
-  authDomain: "react-photography-review.firebaseapp.com",
-  projectId: "react-photography-review",
-  storageBucket: "react-photography-review.appspot.com",
-  messagingSenderId: "993943995887",
-  appId: "1:993943995887:web:93b698a747796f4c7a221a",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
